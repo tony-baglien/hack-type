f10066ded9f7b22a683a8773e20912b90f42dc38
TOny Baglien <tony.b.composition@gmail.com>
2021-05-11 17:30:56 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+let sourceCode = "";
+const textBody = document.querySelector('.textBody');
+const body = document.body;
+
+
+
+
+anime({
+    targets: body,
+    opacity: () => {
+            let random = Math.random() + 0.2
+            if (random > 0.9) {
+                return random - 0.1
+            }
+            return random
+        },
+    loop: true,
+    duration: 75
+})
+
+
+
+
+
+function load_source_code () {
+    var request = new XMLHttpRequest();
+    request.open("GET",'./code.txt');
+    request.onreadystatechange = () => {
+        sourceCode = request.responseText;
+        console.log(sourceCode)
+        
+    }
+    request.send()
+}
+
+
+load_source_code();
+
+let CHAR_SPEED = 5;
+let startIdx = 0;
+let endIdx = 0;
+
+document.addEventListener('keydown', () => {
+    console.log(textBody.scrollHeight, window.innerHeight)
+    textBody.innerText = sourceCode.substring(startIdx,endIdx + CHAR_SPEED)
+    endIdx += CHAR_SPEED;
+    if (endIdx > sourceCode.length) {
+        startIdx = 0;
+        endIdx = 0;
+    }
+    if (textBody.scrollHeight > window.innerHeight) {
+        window.scroll(0,(textBody.scrollHeight + 20))
+    }
+})
+
+
+
